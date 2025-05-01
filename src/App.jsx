47787f7689cdcc13e446cc83317a550c3566ab5c
AleMot94 <Alector666@gmai.com>
2025-05-01 20:49:20 -0300

    dark mode MUI config

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Container, Typography, Button } from '@mui/material'
+import { useColorScheme } from '@mui/material/styles'
+
+function App() {
+  const { mode, setMode } = useColorScheme()
+
+  if (!mode) {
+    return null
+  }
+
+  const toggleMode = () => {
+    setMode(mode === 'light' ? 'dark' : 'light') // Alterna entre 'light' y 'dark'
+  }
+  return (
+    <Container
+      sx={{
+        height: '100vh', // Altura de la ventana
+        overflowY: 'scroll', // Habilita el desplazamiento vertical
+        scrollBehavior: 'smooth', // Desplazamiento suave
+        bgcolor: 'background.default', // Color de fondo del tema
+        color: 'text.primary', // Color del texto del tema
+        p: 2 // Padding
+      }}
+    >
+      <Button variant='contained' onClick={toggleMode}>
+        Cambiar a {mode === 'light' ? 'Modo Oscuro' : 'Modo Claro'}
+      </Button>
+      <Typography variant='h2' gutterBottom>
+        Sección 1
+      </Typography>
+      <Typography variant='body1' paragraph>
+        Contenido de la primera sección...
+      </Typography>
+
+      <Typography variant='h2' gutterBottom>
+        Sección 2
+      </Typography>
+      <Typography variant='body1' paragraph>
+        Contenido de la segunda sección...
+      </Typography>
+
+      <Typography variant='h2' gutterBottom>
+        Sección 3
+      </Typography>
+      <Typography variant='body1' paragraph>
+        Contenido de la tercera sección...
+      </Typography>
+    </Container>
+  )
+}
+
+export default App
